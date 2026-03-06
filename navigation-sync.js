73fe0516bfb739554332595ba5a6f313b639eb9c
PoojaJoijode <154798067+PoojaJoijode@users.noreply.github.com>
2026-03-06 11:35:16 -0500

    Change default filename from index.html to webli1.html

--- navigation-sync.js
+++ navigation-sync.js
@@ -1,29 +1,16 @@
-// navigation-sync.js
 (function () {
   function getFileName() {
-    return location.pathname.split("/").pop() || "index.html";
+    return location.pathname.split("/").pop() || "webli1.html";
   }
 
   function notifyParent() {
     if (window.top !== window.self) {
-      var file = getFileName();
-
-      try {
-        if (window.top.location.hash !== "#" + file) {
-          window.top.history.pushState(null, "", "#" + file);
-        }
-      } catch (e) {
-        // ignore cross-frame/history issues
-      }
-
       try {
         window.top.postMessage(
-          { type: "inner-page-changed", page: file },
+          { type: "inner-page-changed", page: getFileName() },
           "*"
         );
-      } catch (e) {
-        // ignore message errors
-      }
+      } catch (e) {}
     }
   }
 
